b5354954ea9914591e28a717dd97ecb8b6bf20f0
titihanifah <titihanifah25@gmail.com>
2022-07-23 04:32:48 +0700

    merapikan view

--- pages/products.php
+++ pages/products.php
@@ -4,36 +4,34 @@ function filter_add_view_of_the_content( $content ) {
 
   $page_archive_product_id = get_option( 'page_archive_product_id' );
 
-  if ( is_page( 'products' ) ) :
+  if ( is_page( 'products' ) ) {
   $token = oauth_get_token();
 
   $products = get_product_by_api($token);
   get_header();
   ?>
 
-  <div style="width: 90%;">
+  <div style="width: 90%; margin-left:auto; margin-right:auto">
   <?php
-    if( $products > 1 ) :
-  echo '<h3>'.count($products).' data founds.';
+    if( $products > 1 ) {
+      echo '<h3 style="text-align:center">'.count($products).' data founds</h3>';
       foreach( $products as $key => $product ){
         $permalink = site_url('/products/?id='.$product->id);
 
-        echo '<div style="width 33%;">';     
+        echo '<div style="width 70%; text-align:center;">';     
         echo '<br>';
-        echo '<h4><a href="'.$permalink.'">'.($key+1) .'- ('. $product->id . ') ' . $product->name. '</a></h4><br>';
-        echo 'Price ' . $product->originalPrice. '<br>';
-        echo '<image style="width:200px;height:auto;" src="https://uat-api.globaltix.com/api/image?name='. $product->image .'" />';
+        // ($key+1) .'- ('. $product->id . ')
+        echo '<h5><a href="'.$permalink.'">' . $product->name. '</a></h5>';
+        echo '<image style="width:350px;height:auto; margin:15px 0" src="https://uat-api.globaltix.com/api/image?name='. $product->image .'" /><br>';
+        echo '<h6>Price ' . $product->originalPrice. '</h6>';
         echo '<br>';
         echo '</div>';
 
       }
-    else:
+    } else {
       echo 'Error '.$products;
-    endif;
-    ?>
-  </div>
-
-  <?php
-  endif;
+    }
+  echo '</div>';
+  }
   return ob_get_clean();
 }
--- pages/tickettype.php
+++ pages/tickettype.php
@@ -7,20 +7,22 @@ function filter_add_view_of_the_content( $content ) {
   $result = json_decode(get_detail_by_id( $ticket_id, $token, 'ticket' ));
   get_header();
   
+    echo '<br><div class=""><div>';
+  
   if( $ticket_id != 0 ) {
     $detail_tickets = $result->data;
     echo '<h3>Ticket Details:</h3>';
-    echo '<br><div class="row"><div>';
-    echo '<p>name'.$detail_tickets->name.'<p>';
-    echo '<p>variation name'.$detail_tickets->variation->name.'<p>';
-    echo '<p>status '.$detail_tickets->status->name.'<p>';
-    echo '<p>SKU '.$detail_tickets->SKU.'<p>';
-    echo '<p>originalPrice '.$detail_tickets->originalPrice.'<p>';
-    
-    echo '<p>attraction '.$detail_tickets->attraction->title.'<p>';
-    echo '</div></div>';
+    echo '<p>name :'.$detail_tickets->name.'<p>';
+    echo '<p>variation : '.$detail_tickets->variation->name.'<p>';
+    echo '<p>status : '.$detail_tickets->status->name.'<p>';
+    echo '<p>SKU : '.$detail_tickets->SKU.'<p>';
+    echo '<p>originalPrice : '.$detail_tickets->originalPrice.'<p>';    
+    echo '<p>attraction : '.$detail_tickets->attraction->title.'<p>';
+    echo '</div>';
   }
-
+  echo '</div>';
+  echo '<a href="'.site_url('/products').'" class="btn btn-primary">Back to Products</a>';
+  echo '</div>';
 
   return ob_get_clean();
 }
